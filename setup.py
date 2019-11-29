0f4705ee435b7dacee7f1723d7589f3fdfa6f574
jeffliu6068 <50810835+jeffliu6068@users.noreply.github.com>
2019-11-29 14:44:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ta-Chun (Jeff) Liu',                   # Type in your name
   author_email = 'ta-chun.liu@oncology.ox.ac.uk',      # Type in your E-Mail
   url = 'https://github.com/jeffliu6068/GMMchisquare',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jeffliu6068/GMMchisquare/archive/v_001.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jeffliu6068/GMMchisquare/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['GMM', 'Chi-square', 'Categorization'],   # Keywords that define your package best
   install_requires=[            
           'pandas',
